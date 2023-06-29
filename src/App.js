d6302fa933deb1868e26fa447347c9b847d3603a
bennieking05 <bennie@kanarys.com>
2023-06-28 21:44:58 -0500

    Added Tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import './index.css';
 import Employee from './Components/Employee';
 import { useState } from 'react'; 
 function App() {
   const [role, setRole] = useState('dev');
   const showEmployees = true;
   return (
-    <div className="App">
+    <div className="App bg-red-300">
       {showEmployees ? (
       <>
       <input type='text' 
